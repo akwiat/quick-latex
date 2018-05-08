c186287fa5d7b920190097e7dacd11e308b8cd84
akwiat <akwiat@alumni.stanford.edu>
2018-05-08 13:04:49 -0700

    template first try

--- quick_latex.py
+++ quick_latex.py
@@ -1,10 +1,31 @@
 import argparse
+import os
+
+from string import Template
 
 parser = argparse.ArgumentParser(description='quick-compile a problem')
 parser.add_argument('filename', type=str, help='name of the .tex file to compile')
 # parser.add_argument('worksheet_name', nargs='?', type=str, help='name of the worksheet')
 # parser.add_argument('--all', action='store_true', help='update all worksheets')
 
+def ensure_dir(d):
+    try:
+        os.mkdir(d)
+    except:
+        pass
+
 if __name__ == "__main__":
     args = parser.parse_args()
     print("filename: {}".format(args.filename))
+    dir = os.getcwd()
+    tdir = os.path.join(dir, "tex")
+    ensure_dir(tdir)
+
+    base, ext = os.path.splitext(args.filename)
+    outfile = os.path.join(tdir, base + "-main" + ext)
+    with open("quick_template.tex") as f:
+        t = Template(f.read())
+        data = t.substitute(filename=args.filename)
+
+        with open(outfile, "w") as outf:
+            f.write(data)
