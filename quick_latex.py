a28f0c26d1db0048ca2e86918d96d0d786b1f968
Alex Kwiatkowski <alexk614@stanford.edu>
2018-05-10 00:25:59 -0700

    shell

--- quick_latex.py
+++ quick_latex.py
@@ -19,10 +19,10 @@ def ensure_dir(d):
 def compile(f):
     d, base = os.path.split(f)
     os.chdir(d)
-    # cmd = "pdflatex "+base+" &> log.txt"
-    cmd = "ls > log.txt"
+    cmd = "pdflatex "+base+" &> log.txt"
+    # cmd = "ls > log.txt"
     print("running:", cmd)
-    r = subprocess.run(cmd)
+    r = subprocess.run(cmd, shell=True)
     print("output:", r.returncode)
 
 if __name__ == "__main__":
