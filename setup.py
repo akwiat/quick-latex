3e282e936a411f9ed91d42bf338138bfee55c0b3
akwiat <akwiat@alumni.stanford.edu>
2018-05-15 13:35:56 -0700

    rename to qlatex

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.1.dev18',  # Required
+    version='1.0.1.dev19',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -177,7 +177,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            'quick_latex=quick_latex:main',
+            'qlatex=quick_latex:main',
         ],
     },
 
